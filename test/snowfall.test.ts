30c0a8b4616d974bdee891dd123996484e766c6b
EC2 Default User <ec2-user@ip-172-31-64-4.ec2.internal>
2023-01-12 18:29:58 +0000

    Initial commit

---
+++ test/snowfall.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Snowfall from '../lib/snowfall-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/snowfall-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Snowfall.SnowfallStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
